677f28f105aaff3cda144c57058252ed29df1cc2
AmilkarVladimiR <reyesamilkarreyes@gmail.com>
2025-05-11 14:23:48 -0600

    Act5 - Creando método POST en el controlador

--- src/main/java/com/vladimir/personas/Controllers/PersonasController.java
+++ src/main/java/com/vladimir/personas/Controllers/PersonasController.java
@@ -9,19 +9,26 @@ import org.springframework.web.bind.annotation.RestController;
 import com.vladimir.personas.repository.PersonasRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.vladimir.personas.model.PersonasModel;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/personas")
 public class PersonasController {
-      
+      //Metodo get para traer a todas las personas de la base de datos
     @Autowired
     private PersonasRepository personasRepository;
 
     @GetMapping("/traer-personas")
     public List <PersonasModel> TraerPersonas() {
         return personasRepository.findAll();
-        
     }
-    
+    //Metodo para insertar una persona en la base de datos 
+    @PostMapping("/insertar-personas")
+    public PersonasModel insertarPersona(@RequestBody PersonasModel persona) {
+        return personasRepository.save(persona);
+       
+    }
 
 }
